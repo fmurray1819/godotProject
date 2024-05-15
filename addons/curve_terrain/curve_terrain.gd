d4582fe545e31785f36ffb5abfec1be1af4384ba
fmurray1819 <144173154+fmurray1819@users.noreply.github.com>
2024-05-15 12:40:57 -0700

    5/14

--- addons/curve_terrain/curve_terrain.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("Curve_terrain", "Path3D", preload ("path_terrain.gd"), preload ("land.png"))
-
-func _exit_tree():
-	# Clean-up of the plugin goes here.
-	remove_custom_type("Curve_terrain")
